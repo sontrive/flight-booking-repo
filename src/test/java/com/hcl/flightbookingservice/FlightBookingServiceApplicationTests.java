3ab40882cead83d6f4d54d91cbdc9f2d417cf62c
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-07-18 09:43:39 +0530

    Initial commit.

---
+++ src/test/java/com/hcl/flightbookingservice/FlightBookingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hcl.flightbookingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightBookingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
