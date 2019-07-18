a0a4a563dd54ff340eae28a4f0d26a023a06b2a7
pracheerjain <pracheer2002@gmail.com>
2019-07-18 16:33:21 +0530

    PastTravelDetails junit

--- src/test/java/com/hcl/flightbookingservice/controller/PastTravelDetailsControllerTest.java
+++ src/test/java/com/hcl/flightbookingservice/controller/PastTravelDetailsControllerTest.java
@@ -1,21 +1,29 @@
 package com.hcl.flightbookingservice.controller;
 
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.hcl.flightbookingservice.domain.BookingDetailsHistory;
+import com.hcl.flightbookingservice.service.PastTravelDetailsService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PastTravelDetailsControllerTest {
 
 	@InjectMocks
 	PastTravelDetailsController pastTravelDetailsController;
+	
+	@Mock
+	PastTravelDetailsService pastTravelDetailsService;
 	
 	@Before
 	public void setUp() {
@@ -24,7 +32,22 @@ public class PastTravelDetailsControllerTest {
 	
 	@Test
 	public void getBookingHistoryIfUserNameIsValid() {
+		String userName = "sagar"; 
 		List<BookingDetailsHistory> bookingDetails = new ArrayList<>();
-		
+		BookingDetailsHistory bookingDetailsHistory = new BookingDetailsHistory();
+		bookingDetails.add(bookingDetailsHistory);
+		when(pastTravelDetailsService.getPastTravelDetails(userName)).thenReturn(bookingDetails);
+		Assert.assertNotNull(pastTravelDetailsController.getBookingHistory(userName));
+	}
+	
+	@Test
+	public void getBookingHistoryIfUserNameIsBlank() {
+		String userName = ""; 
+		Assert.assertNotNull(pastTravelDetailsController.getBookingHistory(userName));
+	}
+	
+	@Test
+	public void getBookingHistoryIfUserNameIsNull() {
+		Assert.assertNotNull(pastTravelDetailsController.getBookingHistory(null));
 	}
 }
