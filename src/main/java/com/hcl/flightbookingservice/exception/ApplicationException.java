9fdb5b003e37f2e9b6996679a300c9957ced3de6
sontrive <sontrive0310@gmail.com>
2019-07-18 11:13:03 +0530

    commit exception

---
+++ src/main/java/com/hcl/flightbookingservice/exception/ApplicationException.java
@@ -0,0 +1,15 @@
+package com.hcl.flightbookingservice.exception;
+
+	
+	public class ApplicationException extends Exception {
+
+		public ApplicationException() {
+			// TODO Auto-generated constructor stub
+		}
+
+		public ApplicationException(String message) {
+			super(message);
+			// TODO Auto-generated constructor stub
+		}
+
+}
